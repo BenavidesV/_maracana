c56c7cfdfeb123f772de3770ad3f85d21c6c80e2
Jose <benavides@estudiantec.cr>
2021-10-17 19:01:04 -0600

    first approach

---
+++ screens/Login.js
@@ -0,0 +1,193 @@
+import React, { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+// formik
+import { Formik } from 'formik';
+
+import {
+  StyledContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledInputLabel,
+  StyledFormArea,
+  StyledButton,
+  StyledTextInput,
+  LeftIcon,
+  RightIcon,
+  InnerContainer,
+  ButtonText,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  Colors,
+} from './../components/styles';
+import { View, ActivityIndicator } from 'react-native';
+
+//colors
+const { darkLight, brand, primary } = Colors;
+
+// icon
+import { Octicons, Fontisto, Ionicons } from '@expo/vector-icons';
+
+// keyboard avoiding view
+import KeyboardAvoidingWrapper from './../components/KeyboardAvoidingWrapper';
+
+// api client
+import axios from 'axios';
+
+// Async storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// credentials context
+import { CredentialsContext } from './../components/CredentialsContext';
+
+const Login = ({ navigation }) => {
+  const [hidePassword, setHidePassword] = useState(true);
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState();
+
+  // credentials context
+  const {storedCredentials, setStoredCredentials} = useContext(CredentialsContext);
+
+  const handleLogin = (credentials, setSubmitting) => {
+    handleMessage(null);
+    const url = 'https://whispering-headland-00232.herokuapp.com/user/signin';
+    axios
+      .post(url, credentials)
+      .then((response) => {
+        const result = response.data;
+        const { status, message, data } = result;
+
+        if (status !== 'SUCCESS') {
+          handleMessage(message, status);
+        } else {
+          persistLogin({ ...data[0] }, message, status);
+        }
+        setSubmitting(false);
+      })
+      .catch((error) => {
+        setSubmitting(false);
+        handleMessage('An error occurred. Check your network and try again');
+        console.log(error.toJSON());
+      });
+  };
+
+  const handleMessage = (message, type = '') => {
+    setMessage(message);
+    setMessageType(type);
+  };
+
+  // Persisting login
+  const persistLogin = (credentials, message, status) => {
+    AsyncStorage.setItem('flowerCribCredentials', JSON.stringify(credentials))
+      .then(() => {
+        handleMessage(message, status);
+        setStoredCredentials(credentials);
+      })
+      .catch((error) => {
+        handleMessage('Persisting login failed');
+        console.log(error);
+      });
+  };
+
+  return (
+    <KeyboardAvoidingWrapper>
+      <StyledContainer>
+        <StatusBar style="dark" />
+        <InnerContainer>
+          <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')} />
+          <PageTitle>Maracana</PageTitle>
+          <SubTitle>Account Login</SubTitle>
+
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={(values, { setSubmitting }) => {
+              if (values.email == '' || values.password == '') {
+                handleMessage('Por favor llene todos los campos');
+                setSubmitting(false);
+              } else {
+                handleLogin(values, setSubmitting);
+              }
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values, isSubmitting }) => (
+              <StyledFormArea>
+                <MyTextInput
+                  label="Email"
+                  placeholder="andyj@gmail.com"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                  icon="mail"
+                />
+                <MyTextInput
+                  label="Contraseña"
+                  placeholder="* * * * * * * *"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry={hidePassword}
+                  icon="lock"
+                  isPassword={true}
+                  hidePassword={hidePassword}
+                  setHidePassword={setHidePassword}
+                />
+                <MsgBox type={messageType}>{message}</MsgBox>
+
+                {!isSubmitting && (
+                  <StyledButton onPress={handleSubmit}>
+                    <ButtonText>Login</ButtonText>
+                  </StyledButton>
+                )}
+                {isSubmitting && (
+                  <StyledButton disabled={true}>
+                    <ActivityIndicator size="large" color={primary} />
+                  </StyledButton>
+                )}
+
+                <Line />
+
+                <ExtraView>
+                  <ExtraText>¿Aún no tiene cuenta? </ExtraText>
+                  <TextLink onPress={() => navigation.navigate('Signup')}>
+                    <TextLinkContent>Registrarse</TextLinkContent>
+                  </TextLink>
+                </ExtraView>
+              </StyledFormArea>
+            )}
+          </Formik>
+        </InnerContainer>
+      </StyledContainer>
+    </KeyboardAvoidingWrapper>
+  );
+};
+
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon
+          onPress={() => {
+            setHidePassword(!hidePassword);
+          }}
+        >
+          <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
+        </RightIcon>
+      )}
+    </View>
+  );
+};
+
+export default Login;
